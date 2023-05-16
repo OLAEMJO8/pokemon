2fafd2a1594e00b899d47f8aeeb31bb0182d5453
Maria Jose Olaechea <olaecheamariajose@gmail.com>
2023-05-16 05:45:28 -0300

    Copia back

--- api/src/handlers/pokemonsHandler.js
+++ api/src/handlers/pokemonsHandler.js
@@ -1,14 +1,72 @@
-const getAllHandler = (req, res) => {
-  res.status(200).send("Aqui estan todos los pokemons");
+const {
+  pokemonCreate,
+  getOneById,
+  getAllPokemonName,
+  getPokemonName,
+  pokemonArray,
+} = require("../controllers/pokemonsControllers");
+const { Pokemons } = require("../db");
+/////////////////////////////Buscar info
+const getAllHandler = async (req, res) => {
+  try {
+    const response = await Pokemons.findAll();
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
-const getOneHandler = (req, res) => {
-  res.status(200).send("Detalle de id pokemon");
+
+//////////////////////////////Pokemon recibido por id
+
+const getOneHandler = async (req, res) => {
+  const { id } = req.params;
+
+  const source = isNaN(id) ? "bdd" : "api";
+
+  try {
+    const response = await getOneById(id, source);
+
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
-const getNameHandler = (req, res) => {
-  res.status(200).send(" Detalle x pokemon");
+
+//!Buscar pokemon name REVISAR
+
+const getNameHandler = async (req, res) => {
+  const { name } = req.query;
+
+  try {
+    if (name) {
+      const pokemonName = await getPokemonName(name);
+      res.status(200).json(pokemonName);
+    } else {
+      const response = await getAllPokemonName();
+      res.status(200).json(response);
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
-const createNewHandler = (req, res) => {
-  res.status(200).send("Nuevo pokemon creado");
+
+///////////////////////////// Crear pokemon
+const createNewHandler = async (req, res) => {
+  const { id, name, imagen, vida, ataque, defensa } = req.body;
+
+  try {
+    const response = await pokemonCreate(
+      id,
+      name,
+      imagen,
+      vida,
+      ataque,
+      defensa
+    );
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 module.exports = {
