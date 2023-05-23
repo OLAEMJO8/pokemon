eaa58b1401975e89e7d88406c142a66feb4ab867
Maria Jose Olaechea <olaecheamariajose@gmail.com>
2023-05-23 03:58:09 -0300

    Copia Front

--- api/src/controllers/getTypesController.js
+++ api/src/controllers/getTypesController.js
@@ -1,20 +1,28 @@
+// const { Pokemon, Type } = require("../db");
+
+
+
+//   getTypesController = async () => {
+//       const url = await axios.get("https://pokeapi.co/api/v2/type");
+//       const typePokemon = url.data.results.map((p) => p.name);
+//       return typePokemon;
+    
+//   };
+  
+//   module.exports = getTypesController;
 const axios = require("axios");
 const { Pokemon, Type } = require("../db");
 
 getTypesController = async () => {
+  let types = await Type.findAll();
 
-    const url = await axios.get("https://pokeapi.co/api/v2/type");
-    const typePokemon = url.data.results.map((p) => p.name);
-    return typePokemon;
-  
+  if (types.length === 0) {
+    const response = await axios.get("https://pokeapi.co/api/v2/type");
+    types = response.data.results.map((type) => type.name);
+    await Type.bulkCreate(types.map((type) => ({ name: type })));
+  }
+
+  return types;
 };
 
 module.exports = getTypesController;
-
-//   const url = await axios.get("https://pokeapi.co/api/v2/type");
-
-//   const typePokemon = url.data.results.map((p) => p.name);
-//   await typePokemon.forEach(type => Type.create({name: type}))
-
-//   console.log(typePokemon);
-//   return typePokemon;
